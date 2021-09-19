60a315f0dd6cac02abc7fed2135f315af0165808
petroskayange <kayangepetros@gmail.com>
2021-09-19 20:07:05 +0200

    adding idsr reports

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ EmrOhspInterface::Engine.routes.draw do
     resources :radiology, path: 'api/v1/emr_ohsp_interface'      
     get '/get_weeks', to: 'emr_ohsp_interface#weeks_generator'
     get '/get_months', to: 'emr_ohsp_interface#months_generator'
+    get '/generate_weekly_idsr_report', to: 'emr_ohsp_interface#generate_weekly_idsr_report'
+    get '/generate_monthly_idsr_report', to: 'emr_ohsp_interface#generate_monthly_idsr_report'
 end
