8fd2d2080148aeec19406ee3c1e4e176c6c1f2b2
Dominic Kasanga <dominickasanga@gmail.com>
2022-01-06 13:46:10 +0200

    modified

--- app/controllers/emr_ohsp_interface/emr_ohsp_interface_controller.rb
+++ app/controllers/emr_ohsp_interface/emr_ohsp_interface_controller.rb
@@ -13,7 +13,7 @@ class EmrOhspInterface::EmrOhspInterfaceController < ::ApplicationController
     
     def generate_monthly_idsr_report
         render json: service.generate_monthly_idsr_report(params[:request],params[:start_date],params[:end_date]);
-    endgenerate_hmis_15_report
+    end
 
     def generate_hmis_15_report
         render json: service.generate_hmis_15_report(params[:request],params[:start_date],params[:end_date]);
